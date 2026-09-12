ff30253f3a3177804d9103791534c61007255472
Cristian Barreto <barretocristian7@gmail.com>
2026-09-12 16:24:10 -0300

    Morgan logs

--- sistemaIncidencias/backend/server.js
+++ sistemaIncidencias/backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import apiRouter from './src/routes/index.js';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,18 @@ const PORT = process.env.PORT || 3000;
 // Middlewares
 app.use(express.json());
 app.use(cors());
+app.use(
+  morgan((tokens, req, res) => {
+    const timestamp = new Date().toISOString();
+    const method = tokens.method(req, res);
+    const endpoint = tokens.url(req, res);
+    const status = tokens.status(req, res);
+    const responseTime = tokens['response-time'](req, res);
+
+    return `${timestamp} [TFI-PROG-4] ${method} ${endpoint} ${status} ${responseTime} ms`;
+  })
+);
+
 
 // Ruta raíz de prueba
 app.get('/', (req, res) => {
