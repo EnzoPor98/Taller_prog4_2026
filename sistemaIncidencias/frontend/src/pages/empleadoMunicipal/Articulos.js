40a1e67215ca49e3571849a614713a5ce022517c
Cristian Barreto <barretocristian7@gmail.com>
2026-09-23 21:17:39 -0300

    Revert "Merge remote-tracking branch 'origin/main' into uso-jwt"
    This reverts commit 5ff32b64d1ef4688e00009601aa8f003a47067d2, reversing
    changes made to 18e723e9a308dee5f3ee0e95fb85d8dc804c709f.

--- sistemaIncidencias/frontend/src/pages/empleadoMunicipal/Articulos.js
+++ sistemaIncidencias/frontend/src/pages/empleadoMunicipal/Articulos.js
@@ -1,36 +1,67 @@
+const articulos = [
+  {
+    id_articulo: 1,
+    id_area: 3, // Soporte Técnico
+    id_categoria: 12, // Periféricos
+    descripcion: "Monitor LED 24''",
+    activo: 1, // 1 = Disponible
+  },
+  {
+    id_articulo: 2,
+    id_area: 2,
+    id_categoria: 15,
+    descripcion: "Laptop Dell Latitude",
+    activo: 1,
+  },
+  {
+    id_articulo: 3,
+    id_area: 1,
+    id_categoria: 4,
+    descripcion: "Impresora láser multifuncional",
+    activo: 1,
+  },
+  {
+    id_articulo: 4,
+    id_area: 4,
+    id_categoria: 9,
+    descripcion: "Proyector HDMI de sala de reuniones",
+    activo: NaN,
+  },
+  {
+    id_articulo: 5,
+    id_area: 3,
+    id_categoria: 12,
+    descripcion: null,
+    activo: 0,
+  },
+];
+
 document.addEventListener("DOMContentLoaded", () => {
-  cargarTabla();
+  cargarTabla(articulos);
 });
 
-async function cargarTabla() {
-  try {
-    const respuesta = await fetch(
-      "http://localhost:3000/src/pages/empleadoMunicipal/Articulos",
-    );
-    const { articulos } = await respuesta.json();
+function cargarTabla(articulos) {
+  const tbody = document.getElementById("bodyTable");
 
-    const tbody = document.getElementById("bodyTable");
-    tbody.textContent = "";
-    for (let art of articulos) {
-      const atributos_art = [
-        art.id_articulo,
-        art.id_area,
-        art.id_categoria,
-        art.descripcion,
-        art.activo,
-      ];
+  tbody.textContent = "";
 
-      const fila = document.createElement("tr");
+  for (let art of articulos) {
+    const atributos_art = [
+      art.id_articulo,
+      art.id_area,
+      art.id_categoria,
+      art.descripcion,
+      art.activo,
+    ];
 
-      for (let atr of atributos_art) {
-        const columna = document.createElement("td");
-        columna.textContent = atr;
-        fila.appendChild(columna);
-      }
+    const fila = document.createElement("tr");
 
-      tbody.appendChild(fila);
+    for (let atr of atributos_art) {
+      const columna = document.createElement("td");
+      columna.textContent = atr;
+      fila.appendChild(columna);
     }
-  } catch (error) {
-    console.error("Error al conectar con el backend:", error);
+
+    tbody.appendChild(fila);
   }
 }
